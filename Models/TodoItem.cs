0ee99929a81999efa51c9f638379ffe7080fe46b
davidalantodd <david.todd@multiverse.io>
2023-10-21 11:37:25 -0400

    initialize project with .NET Core, create a Todo Model, create database context, and register database context with Dependency Injection container

---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace TodoApi.Models;
+
+//Creating a Todo Model
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Name { get; set; } 
+    public bool IsComplete { get; set; }
+}
